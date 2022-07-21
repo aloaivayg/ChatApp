e68382bb03ff2a9a9b61bd0d1e3b2536ecd36777
thongbui123 <88702681+thongbui123@users.noreply.github.com>
2022-07-21 16:09:34 +0700

    Add Menu bar

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -1,7 +1,88 @@
 package main;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Scanner;
+
+import object.IRepository;
 
 class MainTest {
+    private IRepository iRepository;
+    private Scanner sc = null;
 
+    public void DisplayMenu() {
+        int job = 0;
+        do {
+            this.printMenu();
+            job = sc.nextInt();
+            sc.nextLine();
+            switch (job) {
+                case 0:
+                    doAddUser();
+                    break;
+                case 1:
+                    doUpdateUser();
+                    break;
+                case 2:
+                    doDeleteUser();
+                    break;
+                case 3:
+                    doCreatePrivateGroup();
+                    break;
+                case 4:
+                    doCreatePrivateGroup();
+                    break;
+                case 5:
+                    doCreatePrivateGroup();
+                    break;
+                case 6:
+                    doJoinPublicGroup();
+                    break;
+                case 7:
+                    doSaveData();
+                    break;
+                case 8:
+                    doSaveData();
+                    break;
+                case 9:
+                    doLogout();
+                    break;
+            }
+        } while (job != 9);
+
+    }
+
+    private void doLogout() {
+    }
+
+    private void doSaveData() {
+    }
+
+    private void doJoinPublicGroup() {
+    }
+
+    private void doCreatePrivateGroup() {
+    }
+
+    private void doDeleteUser() {
+    }
+
+    private void doUpdateUser() {
+    }
+
+    private void doAddUser() {
+    }
+
+    private void printMenu() {
+        System.out.println("");
+        System.out.println("0. Add new user");
+        System.out.println("1. Edit information of user");
+        System.out.println("2. Delete a user");
+        System.out.println("3. Create a new chat group");
+        System.out.println("4. Delete a chat group");
+        System.out.println("5. Join your private chat group");
+        System.out.println("6. Join the public chat group");
+        System.out.println("7. Save the data");
+        System.out.println("8. Restore the data");
+        System.out.println("9. Log out and exit");
+        System.out.print("What do you want? ");
+    }
 }
