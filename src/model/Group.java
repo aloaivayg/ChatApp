1c19a4a87cb6a742f6a5c73d58d51e165aac2930
thongbui123 <88702681+thongbui123@users.noreply.github.com>
2022-07-28 15:53:25 +0700

    Rename, Edit Group Repo

--- src/model/Group.java
+++ src/model/Group.java
@@ -4,9 +4,32 @@ import java.util.List;
 
 public abstract class Group {
     private String id;
+    private int groupNumber;
     private List<User> userList;
 
     public Group() {
 
     }
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List<User> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+
+	public int getGroupNumber() {
+		return userList.size();
+	}
+    
+    
 }
